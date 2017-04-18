da5c9f01fd674865ac0c7486e4329394405957fc
Jothi Mala <jothimala96@gmail.com>
2017-04-18 16:10:15 +0530

    Update ideone_ufeOxk.c

--- ideone_ufeOxk.c
+++ ideone_ufeOxk.c
@@ -6,8 +6,11 @@ int main(void) {
 	if(a=='A'||a=='E'||a=='I'||a=='O'||a=='U'||a=='a'||a=='e'||a=='i'||a=='o'||a=='u'){
 		printf("Vowel");
 	}
-		else 
+		else if(a>=65&&a<=90||a>=97&&a<=122){
 		printf("Consonant");
+		}
+	else
+		printf("Invalid");
 	
 	// your code goes here
 	return 0;
